41ff29cd2217f4dac28c83a16aa96453ccfb4277
Rachel Moser <moser.rachel@gmail.com>
2020-11-06 17:21:36 -0600

    add Comments and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
   validates :username, presence: true, length: { in: 4..14 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
-
-  has_many :posts
 end
