d72d238f37afc052afcab8a20ad0fef3c4420312
Rachel Moser <moser.rachel@gmail.com>
2020-11-06 17:04:16 -0600

    add Posts and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 4..14 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  has_many :posts
 end
